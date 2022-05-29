f354fb0dbf81fd623e3600391f4881303014f28c
suye <3405187813@qq.com>
2022-05-29 23:27:29 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -0,0 +1,19 @@
+package com.atguigu.git;
+
+/**
+ * @author layne
+ */
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+        System.out.println("master test!");
+        System.out.println("hot-fix test!");
+        System.out.println("push test!");
+        System.out.println("pull test!");
+        System.out.println("gitee test!");
+        System.out.println("gitee test2!");
+    }
+}
